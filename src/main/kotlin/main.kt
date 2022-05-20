6ec80c352370f9665c861215c48c57d60cb06f5c
Алексей Доронин <ziban1703@yandex.ru>
2022-05-20 14:00:30 +0300

    Modification Commit

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,41 +1,66 @@
 data class Post(
-    val id: Int,
-    val ownerId: Int,
-    val fromId: Int,
-    val createdBy: Int,
-    val date: Int,
-    val text: String,
-    val replyOwnerId: Int,
-    val replyPostId: Int,
-    val friendsOnly: Boolean,
-    val commentsCount: Int,
-    val commentsCanPost: Boolean,
-    val commentsGroupsCanPost: Boolean,
-    val commentsCanClose: Boolean,
-    val commentsCanOpen: Boolean,
-    val copyrightId: Int,
-    val copyrightLink: String,
-    val copyrightName: String,
-    val copyrightType: String,
-    val likesCount: Int,
-    val likesUserLikes: Boolean,
-    val likesCanLike: Boolean,
-    val likesCanPublish: Boolean,
-    val repostsCount: Int,
-    val repostsUserReposted: Boolean,
-    val viewsCount: Int,
-    val postType: String,
-    val signerId: Int,
-    val canPin: Boolean,
-    val canDelete: Boolean,
-    val canEdit: Boolean,
-    val isPinned: Boolean,
-    val markedAsAd: Boolean,
-    val isFavorite: Boolean,
-    val isDonut: Boolean,
-    val donutPaidDuration: Int,
-    val donutPlaceholder: String,
-    val donutCanPublishFreeCopy: Boolean,
-    val donutEditMode: String,
-    val postponedId: Int,
+    val id: Int = 0,
+    val ownerId: Int = 0,
+    val fromId: Int = 0,
+    val createdBy: Int = 0,
+    val date: Int = 0,
+    val text: String = "",
+    val replyOwnerId: Int = 0,
+    val replyPostId: Int = 0,
+    val friendsOnly: Boolean = true,
+    val comments: Comment = Comment(),
+    val copyright: Copyright = Copyright(),
+    val likes: Likes = Likes(),
+    val reposts: Reports = Reports(),
+    val views: Views = Views(),
+    val postType: String = "",
+    val signerId: Int = 0,
+    val canPin: Boolean = true,
+    val canDelete: Boolean = true,
+    val canEdit: Boolean = true,
+    val isPinned: Boolean = true,
+    val markedAsAd: Boolean = true,
+    val isFavorite: Boolean = true,
+    val donut: Donut = Donut(),
+    val postponedId: Int = 0,
+)
+
+data class Comment(
+    val count: Int = 0,
+    val canPost: Boolean = true,
+    val groupsCanPost: Boolean = true,
+    val canClose: Boolean = true,
+    val canOpen: Boolean = true,
+
+    )
+
+data class Copyright(
+    val id: Int = 0,
+    val link: String = "",
+    val name: String = "",
+    val type: String = "",
+)
+
+data class Likes(
+    val count: Int = 0,
+    val userLikes: Boolean = true,
+    val canLike: Boolean = true,
+    val canPublish: Boolean = true,
+)
+
+data class Reports(
+    val count: Int = 0,
+    val userReposted: Boolean = true,
+)
+
+data class Views(
+    val count: Int = 0,
+)
+
+data class Donut(
+    val isDonut: Boolean = true,
+    val paidDuration: Int = 0,
+    val placeholder: String = "",
+    val canPublishFreeCopy: Boolean = true,
+    val editMode: String = "",
 )
--- src/test/kotlin/WallServiceTest.kt
+++ src/test/kotlin/WallServiceTest.kt
@@ -7,102 +7,37 @@ class WallServiceTest {
     @Test
     fun add() {
         val service = WallService()
-        val post = service.add(Post(0, 1, 1, 1, 1, "", 1, 1,
-            true, 1, true, true, true,
-            true, 1, "", "", "", 1,
-            true, true, true, 1, true, 1,
-            "", 1, true, true, true, true, true, true,
-            true, 1, "", true, "", 1))
+        val post = service.add(Post())
 
         val result = post.id
-        assertEquals(1,result)
+        assertEquals(1, result)
 
     }
 
     @Test
     fun updateTrue() {
         val service = WallService()
-        service.add(Post(0, 1, 1, 1, 1, "", 1, 1,
-            true, 1, true, true, true,
-            true, 1, "", "", "", 1,
-            true, true, true, 1, true, 1,
-            "", 1, true, true, true, true, true, true,
-            true, 1, "", true, "", 1))
-        service.add(Post(0, 1, 1, 1, 1, "", 1, 1,
-            true, 1, true, true, true,
-            true, 1, "", "", "", 1,
-            true, true, true, 1, true, 1,
-            "", 1, true, true, true, true, true, true,
-            true, 1, "", true, "", 1))
-        service.add(Post(0, 1, 1, 1, 1, "", 1, 1,
-            true, 1, true, true, true,
-            true, 1, "", "", "", 1,
-            true, true, true, 1, true, 1,
-            "", 1, true, true, true, true, true, true,
-            true, 1, "", true, "", 1))
-        service.add(Post(0, 1, 1, 1, 1, "", 1, 1,
-            true, 1, true, true, true,
-            true, 1, "", "", "", 1,
-            true, true, true, 1, true, 1,
-            "", 1, true, true, true, true, true, true,
-            true, 1, "", true, "", 1))
+        service.add(Post())
+        service.add(Post())
+        service.add(Post())
+        service.add(Post())
 
-        // создаём информацию об обновлении
-        val update = Post(4, 0, 0, 0, 0, "", 0, 0,
-            false, 0, false, false, false,
-            false, 1, "", "", "", 1,
-            true, true, true, 1, true, 1,
-            "", 1, true, true, true, true, true, true,
-            true, 1, "", true, "", 1)
-
-        // выполняем целевое действие
+        val update = Post(4, 1, 1, 1, 1)
         val result = service.update(update)
-
-        // проверяем результат (используйте assertTrue или assertFalse)
         assertTrue(result)
     }
 
     @Test
-    fun updateFalse(){
+    fun updateFalse() {
 
         val service = WallService()
-        service.add(Post(0, 1, 1, 1, 1, "", 1, 1,
-            true, 1, true, true, true,
-            true, 1, "", "", "", 1,
-            true, true, true, 1, true, 1,
-            "", 1, true, true, true, true, true, true,
-            true, 1, "", true, "", 1))
-        service.add(Post(0, 1, 1, 1, 1, "", 1, 1,
-            true, 1, true, true, true,
-            true, 1, "", "", "", 1,
-            true, true, true, 1, true, 1,
-            "", 1, true, true, true, true, true, true,
-            true, 1, "", true, "", 1))
-        service.add(Post(0, 1, 1, 1, 1, "", 1, 1,
-            true, 1, true, true, true,
-            true, 1, "", "", "", 1,
-            true, true, true, 1, true, 1,
-            "", 1, true, true, true, true, true, true,
-            true, 1, "", true, "", 1))
-        service.add(Post(0, 1, 1, 1, 1, "", 1, 1,
-            true, 1, true, true, true,
-            true, 1, "", "", "", 1,
-            true, true, true, 1, true, 1,
-            "", 1, true, true, true, true, true, true,
-            true, 1, "", true, "", 1))
+        service.add(Post())
+        service.add(Post())
+        service.add(Post())
+        service.add(Post())
 
-        // создаём информацию об обновлении
-        val update = Post(10, 0, 0, 0, 0, "", 0, 0,
-            false, 0, false, false, false,
-            false, 1, "", "", "", 1,
-            true, true, true, 1, true, 1,
-            "", 1, true, true, true, true, true, true,
-            true, 1, "", true, "", 1)
-
-        // выполняем целевое действие
+        val update = Post(6, 1, 1, 1, 1)
         val result = service.update(update)
-
-        // проверяем результат (используйте assertTrue или assertFalse)
         assertFalse(result)
     }
 }
